f7a05a116bbd6c267f8bb576135d0b04b57ef812
= <=>
2024-12-18 14:14:11 -0500

    fix inputs

--- veriables.tf
+++ veriables.tf
@@ -16,7 +16,10 @@ variable "delay_after_environment_creation" {
 variable "environments" {
   type = map(object({
     subscription_id = string
+    tenant_id       = string
+    client_id       = string
     branch_name     = string
+    reviewers       = list(string)
     backend = object({
       resource_group_name  = string
       storage_account_name = string
